bcb31451e9b70911a7165a0d858b15b2c1b0166b
Christian Petragnani <112440144+Cpetrag1@users.noreply.github.com>
2022-10-10 01:35:34 +0000

    milestone 1

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -1,23 +1,43 @@
 import pygame
 import random
-
+import math
+import time
 pygame.init()
-surface = pygame.display.set_mode((500,500))
-color = (0,0,255)
-ccolor = (255,0,0)
-pygame.draw.rect(surface, color, pygame.Rect(0,0,500,500))
-pygame.draw.circle(surface, ccolor, (250,250), 500)
+surface = pygame.display.set_mode((400,400))
+def dartboard():
+  pygame.draw.rect(surface, "blue", pygame.Rect(0,0,400,400))
+  pygame.draw.circle(surface, "red", (200,200), 200)
+  pygame.draw.line(surface, "black", (200,0), (200,400))
+  pygame.draw.line(surface, "black", (0,200), (400,200))
+dartboard()
 pygame.display.flip()
 for i in range(11):
-  x = random.randrange(0,501)
-  y = random.randrange(0,501)
-  distance_from_center = math.hypot(x1-x2, y1-y2) #the distance formula
-  is_in_circle = distance_from_center <= width/2 #screen width
+  x = random.randrange(0,401)
+  y = random.randrange(0,401)
+  distance_from_center = math.hypot(200-x, 200-y) #the distance formula
+  is_in_circle = distance_from_center <= 400/2 #screen width
   if is_in_circle:
-    c = (0,255,0)
+      pygame.draw.circle(surface, "green", (x,y), 3)
   else:
-    c = (150,0,0)
-  pygame.draw.circle(surface, c, (x,y), 3)
-  pygame.display.flip()
+      pygame.draw.circle(surface, "white", (x,y), 3)
+pygame.display.flip()
+time.sleep(3)
+dartboard()
+pygame.draw.rect(surface, "blue", pygame.Rect(50,200,100,100))
+pygame.draw.rect(surface, "red4", pygame.Rect(250,200,100,100))
+pygame.display.flip()
+print("Press on blue if you think player1 wins and red if you think player2 wins!")
 
-  
+for event in pygame.event.get():
+  if event.pos[0] > 50 & event.pos[0] < 150 & event.pos[1] > 200 & event.pos[1] < 300:
+    print("You chose player1 to win")
+  if event.pos[0] > 250 & event.pos[0] < 350 & event.pos[1] > 200 & event.pos[1] < 300:
+    print("You chose player2 to win")
+# for event in pygame.event.get():
+#   if event.type == pygame.MOUSEBUTTONDOWN:
+#     x = event.pos[0]
+#     y = event.pos[1]
+#   if x > 50 & x < 150 & y > 200 & y < 300:
+#     print("you chose player1")
+#   if x > 250 & x < 350 & y > 200 & y < 300:
+#     print("you chose player2")  
--- ch04/milestoneI/ideas.md
+++ ch04/milestoneI/ideas.md
@@ -18,12 +18,12 @@ For each one below you should have a basic description of a program idea, a coup
 
 ## Project Idea 1
 
-< add your idea here >
+< Working calculator using the mouse and not text input >
 
 ## Project Idea 2
 
-< add your idea here >
+< An user input stroyline >
 
 ## Project Idea 3
 
-< add your idea here >
+< mario kart type game with a driving focus >
